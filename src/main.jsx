bae775f54206ee47e1ef7bae860ac99526685e5a
Achugwo Michael <139189514+MichaelAchugwo@users.noreply.github.com>
2024-04-29 14:18:44 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import LoginPage from "./components/LoginPage";
-import SuccessPage from "./components/SuccessPage";
 import LogoutPage from "./components/LogoutPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -15,7 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/home" element={<App />} />
         <Route path="/" element={<LoginPage />} />
         <Route path="/logout" element={<LogoutPage />} />
-        <Route path="/success" element={<SuccessPage />} />
       </Routes>
     </Router>
   </React.StrictMode>
